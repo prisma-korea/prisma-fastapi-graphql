610d7154cf305130f4e17ef15454feb4a19b6b87
hyochan <dooboolab@gmail.com>
2022-06-19 12:37:32 +0900

    Configure jwt auth on rest apis

--- src/apis/users.py
+++ src/apis/users.py
@@ -1,17 +1,40 @@
-from fastapi import APIRouter
+from typing import List
+from fastapi import APIRouter, Depends
+from src.models.User import User
+from src.prisma import prisma
+from src.utils.auth import JWTBearer, decodeJWT
 
 router = APIRouter()
 
+
 @router.get("/users/", tags=["users"])
 async def read_users():
-    return [{"username": "Rick"}, {"username": "Morty"}]
+    users: List[User] = await prisma.user.find_many()
+
+    for (user) in users:
+        del user.password
+
+    return users
 
 
 @router.get("/users/me", tags=["users"])
-async def read_user_me():
-    return {"username": "fakecurrentuser"}
+async def read_user_me(token=Depends(JWTBearer())):
+    decoded = decodeJWT(token)
+
+    if 'userId' in decoded:
+        userId = decoded['userId']
+        return await prisma.user.find_unique(
+            where={
+                "id": userId
+            }
+        )
+    return None
 
 
-@router.get("/users/{username}", tags=["users"])
-async def read_user(username: str):
-    return {"username": username}
+@router.get("/users/{userId}", tags=["users"])
+async def read_user(userId: str):
+    user = await prisma.user.find_unique(
+        where={"id": userId}
+    )
+
+    return user
--- src/utils/auth.py
+++ src/utils/auth.py
@@ -1,23 +1,57 @@
 from datetime import time
 import os
-from typing import Dict
-
+from typing_extensions import Self
 import jwt
+from typing import Dict
+from fastapi import Request, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
 jwtSecret = os.environ.get('JWT_SECRET')
+
 
 def signJWT(user_id: str) -> Dict[str, str]:
     payload = {
         "userId": user_id,
-        "expires": time() + 600
+        "expires": time().second + 600
     }
     token = jwt.encode(payload, jwtSecret, algorithm='HS256')
 
     return token
 
+
 def decodeJWT(token: str) -> dict:
     try:
         decoded = jwt.decode(token, jwtSecret, algorithms=['HS256'])
-        return decoded if decoded["expires"] >= time() else None
+        return decoded if decoded["expires"] >= time().second else None
     except:
         return {}
+
+
+class JWTBearer(HTTPBearer):
+    def __init__(self, auto_error: bool = True):
+        super(JWTBearer, self).__init__(auto_error=auto_error)
+
+    async def __call__(self, request: Request):
+        credentials: HTTPAuthorizationCredentials = await super(JWTBearer, self).__call__(request)
+        if credentials:
+            if not credentials.scheme == "Bearer":
+                raise HTTPException(
+                    status_code=403, detail="Invalid authentication scheme.")
+            if not self.verify_jwt(credentials.credentials):
+                raise HTTPException(
+                    status_code=403, detail="Invalid token or expired token.")
+            return credentials.credentials
+        else:
+            raise HTTPException(
+                status_code=403, detail="Invalid authorization code.")
+
+    def verify_jwt(self, jwtToken: str) -> bool:
+        isTokenValid: bool = False
+
+        try:
+            payload = decodeJWT(jwtToken)
+        except:
+            payload = None
+        if payload:
+            isTokenValid = True
+        return isTokenValid
