39854a329c77d085aca839b85e9c163f528b9804
hyochan <dooboolab@gmail.com>
2022-06-20 15:22:07 +0900

    Refactor tests

--- test/test_main.py
+++ test/test_main.py
@@ -1,11 +1,12 @@
 from fastapi.testclient import TestClient
+from prisma import Prisma
 
 from main import app
 
 client = TestClient(app)
+prisma = Prisma()
 
 
-def test_read_item():
+def test_root():
     response = client.get("/")
     assert response.status_code == 200
-
